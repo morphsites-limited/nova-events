41316b0767994da3577135be966e84d92860f2ad
Daniel Crewdson <daniel.crewdson@dewsign.co.uk>
2019-08-16 17:13:47 +0100

    better output

--- src-php/Resources/views/index.blade.php
+++ src-php/Resources/views/index.blade.php
@@ -1,5 +1,19 @@
 <div>
     @foreach($events as $event)
-        <h1>{{ $event->title }}</h1>
+        <div>
+            <h2>{{ $event->title }}</h2>
+            <h3>Info</h3>
+            <p>{{ $event->long_desc ?? $event->short_desc ?? 'No description.' }}</p>
+            <div>
+                <h3>When?</h3>
+                <p>From: {{ $event->start_date->format('d/m/Y') }}</p>
+                <p>To: {{ $event->end_date->format('d/m/Y') }}</p>
+            </div>
+            <div>
+                <h3>Where?</h3>
+                <p>{{ $event->location->title }}</p>
+            </div>
+        </div>
+
     @endforeach
 </div>
