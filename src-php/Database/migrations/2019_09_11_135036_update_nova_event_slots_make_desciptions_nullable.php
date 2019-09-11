bbb3a8883fb2a0b942e47c21ffb2487f33888979
Daniel Crewdson <daniel.crewdson@dewsign.co.uk>
2019-09-11 16:28:19 +0100

    fix broken class name

--- src-php/Database/migrations/2019_09_11_135036_update_nova_event_slots_make_desciptions_nullable.php
+++ src-php/Database/migrations/2019_09_11_135036_update_nova_event_slots_make_desciptions_nullable.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateNovaEventSlotsTableAddLocationId extends Migration
+class UpdateNovaEventSlotsMakeDesciptionsNullable extends Migration
 {
     /**
      * Run the migrations.
