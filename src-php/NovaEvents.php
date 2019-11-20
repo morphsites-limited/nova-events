3560dc5a93d72569177eb8744d3347006fb4af3e
Daniel Crewdson <daniel.crewdson@dewsign.co.uk>
2019-11-20 16:24:14 +0000

    add maxfactor support sitemap functionality

--- src-php/NovaEvents.php
+++ src-php/NovaEvents.php
@@ -4,9 +4,43 @@ namespace Dewsign\NovaEvents;
 
 use Dewsign\NovaEvents\Models\Event;
 use Illuminate\Support\Facades\File;
+use Dewsign\NovaEvents\Models\EventCategory;
 
 class NovaEvents
 {
+    public static function sitemap($sitemap)
+    {
+        static::addIndex($sitemap);
+        static::addCategories($sitemap);
+        static::addEvents($sitemap);
+    }
+
+    public static function addIndex($sitemap)
+    {
+        $sitemap->add(route('events.index'));
+    }
+
+    public static function addCategories($sitemap)
+    {
+        app(config('nova-events.models.category', EventCategory::class))::active()
+            ->has('events')
+            ->get()
+            ->each(function ($category) use ($sitemap) {
+                $sitemap->add(route('events.list', $category));
+            });
+
+    }
+
+    public static function addEvents($sitemap)
+    {
+        app(config('nova-events.models.events', Event::class))::active()
+            ->has('categories')
+            ->get()
+            ->each(function ($event) use ($sitemap) {
+                $sitemap->add(route('events.show', [$event->primaryCategory, $event]));
+            });
+    }
+
     public static function availableTemplates()
     {
         $packageTemplatePath = __DIR__ . '/Resources/views/templates';
