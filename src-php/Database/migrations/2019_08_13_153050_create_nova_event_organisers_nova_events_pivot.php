78d78cf274a9cbb77b064c7f5cd1c233fd0eb646
Daniel Crewdson <daniel.crewdson@dewsign.co.uk>
2020-05-12 13:21:50 +0100

    remove nullable from the event - organisers pivot as the foreign cell is not nullable and was causing errors

--- src-php/Database/migrations/2019_08_13_153050_create_nova_event_organisers_nova_events_pivot.php
+++ src-php/Database/migrations/2019_08_13_153050_create_nova_event_organisers_nova_events_pivot.php
@@ -14,9 +14,9 @@ class CreateNovaEventOrganisersNovaEventsPivot extends Migration
     public function up()
     {
         Schema::create('nova_event_organisers_nova_events', function (Blueprint $table) {
-            $table->integer('organiser_id')->unsigned()->nullable();
+            $table->integer('organiser_id')->unsigned();
             $table->foreign('organiser_id')->references('id')->on('nova_event_organisers')->onDelete('cascade');
-            $table->integer('nova_event_id')->unsigned()->nullable();
+            $table->integer('nova_event_id')->unsigned();
             $table->foreign('nova_event_id')->references('id')->on('nova_events')->onDelete('cascade');
             $table->primary(['organiser_id', 'nova_event_id'], 'event_organiser_primary');
         });
